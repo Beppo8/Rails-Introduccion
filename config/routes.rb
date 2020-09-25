f7448721d32c6d73e7515ef09e62fdd58041a25d
Beppo <beppo@localhost.localdomain>
2020-09-25 11:15:00 -0500

    upload images

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,22 @@ Rails.application.routes.draw do
   resources :categories
   devise_for :users
   get "/bienvenida", to: "home#index"
+  get "perfil", to:"users#edit"
+  resources :users, only: [:update]
 
   root to: "home#index"
+  resources :categories
+  resources :article do
+    get "articles/user/:user_id", to: "articles#from_author", on: :collection
+  end
 
-  get "articles/user/:user_id", to: "articles#from_author"
 
-  get "articles", to:"articles#index"  
-  get "articles/new", to: "articles#new", as: :new_articles
-  get "articles/:id", to: "articles#show"
-  get "articles/:id/edit", to: "articles#edit"
+  # get "articles", to:"articles#index"  
+  # get "articles/new", to: "articles#new", as: :new_articles
+  # get "articles/:id", to: "articles#show"
+  # get "articles/:id/edit", to: "articles#edit"
 
-  patch "articles/:id", to: "articles#update", as: :article
-  post "articles", to: "articles#create"
-  delete "articles/:id", to:"articles#destroy"
+  # patch "articles/:id", to: "articles#update", as: :article
+  # post "articles", to: "articles#create"
+  # delete "articles/:id", to:"articles#destroy"
 end
